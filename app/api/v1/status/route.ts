6ef4a77517a3d1191aa029be1d251984268a4e3b
germano aquino <germano.r.aquino@gmail.com>
2026-07-30 13:05:44 -0300

    add: status endpoint

---
+++ app/api/v1/status/route.ts
@@ -0,0 +1,35 @@
+import database from "@/infra/database";
+import { withErrorHandler } from "@/infra/with-error-handler";
+import { NextResponse } from "next/server";
+
+export const GET = withErrorHandler(async (request: Request) => {
+  const updatedAt = new Date().toISOString();
+
+  const versionResult = await database.query({ text: "SHOW server_version;" });
+  const version = versionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query({
+    text: "SHOW max_connections;",
+  });
+  const maxConnections = Number(maxConnectionsResult.rows[0].max_connections);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnectionsResult = await database.query({
+    text: "SELECT * FROM pg_stat_activity WHERE state = 'active' AND datname = $1;",
+    values: [databaseName],
+  });
+  const openedConnections = openedConnectionsResult.rowCount;
+
+  const statusBody = {
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version,
+        max_connections: maxConnections,
+        opened_connections: openedConnections,
+      },
+    },
+  };
+
+  return NextResponse.json(statusBody, { status: 200 });
+});
---
+++ tests/integration/v1/status/get.test.ts
@@ -0,0 +1,16 @@
+describe("GET api/v1/status", () => {
+  test("Retriving current system status", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+
+    expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+
+    expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+    expect(responseBody.dependencies.database.version).toBe("16.12");
+    expect(responseBody.dependencies.database.opened_connections).toBe(1);
+    expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(
+      1,
+    );
+  });
+});
